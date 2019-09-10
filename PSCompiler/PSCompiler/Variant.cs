b414722859b750dcc67c6bedb2e471e8574ad752
mr7falcon <mr.falcon.inc@gmail.com>
2019-09-10 23:02:46 +0300

    Virtual Machine prototype

--- PSCompiler/PSCompiler/Variant.cs
+++ PSCompiler/PSCompiler/Variant.cs
@@ -5,12 +5,49 @@ namespace PSCompiler
     [StructLayout(LayoutKind.Explicit, Size = 8)]
     public class Variant
     {
+        public static readonly ushort size = 8;
+
         [FieldOffset(0)]
         private double value;
 
         public Variant(double _value = 0.0)
         {
             this.value = _value;
+        }
+
+        public static Variant operator +(Variant op1, Variant op2)
+        {
+            return new Variant(op1.value + op2.value);
+        }
+
+        public static Variant operator -(Variant op1, Variant op2)
+        {
+            return new Variant(op1.value - op2.value);
+        }
+
+        public static bool operator >(Variant op1, Variant op2)
+        {
+            return op1.value > op2.value;
+        }
+
+        public static bool operator <(Variant op1, Variant op2)
+        {
+            return op1.value < op2.value;
+        }
+
+        public static bool operator ==(Variant op1, Variant op2)
+        {
+            return op1.value == op2.value;
+        }
+
+        public static bool operator !=(Variant op1, Variant op2)
+        {
+            return op1.value != op2.value;
+        }
+
+        public static implicit operator Variant(double var)
+        {
+            return new Variant(var);
         }
     }
 }
--- VirtualMachine/VirtualMachine/VirtualMachine.cs
+++ VirtualMachine/VirtualMachine/VirtualMachine.cs
@@ -13,6 +13,7 @@ namespace VirtualMachine
         ADD,
         SUB,
         LT,
+        GT,
         JZ,
         JNZ,
         JMP,
@@ -23,11 +24,127 @@ namespace VirtualMachine
 
     class VirtualMachine
     {
+        private readonly byte[] program;
+        private readonly uint size;
         private Stack<Variant> stack = new Stack<Variant>();
+        //private Dictionary<uint, Variant> vars = new Dictionary<uint, Variant>();
 
-        public void Run(byte[] program)
+        public VirtualMachine(byte[] _program, uint _size)
         {
+            program = _program;
+            size = _size;
+        }
 
+        private Variant GetVar(ref uint pos)
+        {
+            if (pos + Variant.size < size)
+            {
+                Variant arg = new Variant(BitConverter.ToDouble(program, (int)pos));
+                pos += Variant.size;
+                return arg;
+            }
+            else
+            {
+                throw new Exception("stack overflow");
+            }
+        }
+
+        private void JmpToMark(ref uint pos)
+        {
+            uint mark = BitConverter.ToUInt32(program, (int)pos);
+            if (mark < size)
+            {
+                pos = mark;
+            }
+        }
+
+        public void Run()
+        {
+            for (uint i = 0; i < size; ++i)
+            {
+                switch((ByteCommand)program[i])
+                {
+                    case ByteCommand.FETCH:
+                        {
+                            Variant var = GetVar(ref i);
+                            stack.Push(var);
+                            //vars.Add(var);
+                            break;
+                        }
+                    case ByteCommand.STORE:
+                        { 
+                            //vars.Add(stack.Pop());
+                            break;
+                        }
+                    case ByteCommand.PUSH:
+                        {
+                            stack.Push(GetVar(ref i));
+                            break;
+                        }
+                    case ByteCommand.POP:
+                        {
+                            stack.Pop();
+                            break;
+                        }
+                    case ByteCommand.ADD:
+                        {
+                            Variant op1 = stack.Pop();
+                            Variant op2 = stack.Pop();
+                            stack.Push(op1 + op2);
+                            break;
+                        }
+                    case ByteCommand.SUB:
+                        {
+                            Variant op1 = stack.Pop();
+                            Variant op2 = stack.Pop();
+                            stack.Push(op1 - op2);
+                            break;
+                        }
+                    case ByteCommand.LT:
+                        {
+                            Variant op1 = stack.Pop();
+                            Variant op2 = stack.Pop();
+                            Variant res = new Variant(op1 < op2 ? 1.0 : 0.0);
+                            stack.Push(res);
+                            break;
+                        }
+                    case ByteCommand.GT:
+                        {
+                            Variant op1 = stack.Pop();
+                            Variant op2 = stack.Pop();
+                            Variant res = new Variant(op1 > op2 ? 1.0 : 0.0);
+                            stack.Push(res);
+                            break;
+                        }
+                    case ByteCommand.JZ:
+                        {
+                            if (stack.Pop() == (Variant)(0.0))
+                            {
+                                JmpToMark(ref i);
+                            }
+                            break;
+                        }
+                    case ByteCommand.JNZ:
+                        {
+                            if (stack.Pop() != (Variant)(0.0))
+                            {
+                                JmpToMark(ref i);
+                            }
+                            break;
+                        }
+                    case ByteCommand.JMP:
+                        {
+                            JmpToMark(ref i);
+                            break;
+                        }
+                    case ByteCommand.HALT:
+                        {
+                            return;
+                        }
+                    default:
+                        throw new Exception("byte error");
+                }
+            }
         }
     }
 }
